cd4268eda84d363665cdfa981feefb639b805a17
Dean <72749793+drossano@users.noreply.github.com>
2022-10-05 12:59:00 -0400

    Pass test for verse 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,15 @@
 class Bottles
-  def verse(_)
-    "99 bottles of beer on the wall, " + 
-    "99 bottles of beer.\n" +
+  def verse(number)
+    if number == 2
+    "2 bottles of beer on the wall, " + 
+    "2 bottles of beer.\n" +
     "Take one down and pass it around, " + 
-    "98 bottles of beer on the wall.\n"
+    "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " + 
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " + 
+      "#{number - 1} bottles of beer on the wall.\n"
+    end
   end
 end
