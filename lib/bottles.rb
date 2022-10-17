7b0689dc1a7c5e4fcdc62865945b3f65222bc69a
Dean <72749793+drossano@users.noreply.github.com>
2022-10-17 18:36:53 -0400

    Fix demeter violation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -14,7 +14,7 @@ class Bottles
   end
 
   def verse(number)
-    verse_template.new(number).lyrics
+    verse_template.lyrics(number)
   end
 end
 
@@ -75,6 +75,10 @@ class BottleVerse
 
   def initialize(number)
     @number = number
+  end
+
+  def self.lyrics(number)
+    new(number).lyrics
   end
 
   def lyrics
